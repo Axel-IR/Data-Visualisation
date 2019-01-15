aab96e2784e5cee0f0e9b6817feac2bcc6df123a
Axel Iradukunda <axel.iradukunda@student.ehb.be>
2019-01-15 18:39:04 +0100

    Assignment2
    Twitter api: ophalen van tweets en tonen met p5

---
+++ Assignment 2/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+     consumer_key:         'uJYLBmHHJLbTz5ZGOGKD0oCZx',
+  consumer_secret:      'JdRz4jFzDVMx0AMi1CHvZkFpOeCHdUSDTwbuIuf3rfjtoKSUi6',
+  access_token:         '1079393336912478209-4nlf6mY2Y2MK85y1Mzr68RpBBty8ma',
+  access_token_secret:  'tgF2dpL5sVXcmcG99fDpyBJNRX8DI36ZmMyPsIWguCpJ9',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
