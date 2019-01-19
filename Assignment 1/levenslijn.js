3194f3370872674e437641d3fdc4d47940251979
Axel Iradukunda <axel.iradukunda@student.ehb.be>
2019-01-19 21:46:27 +0100

    Comments
    I added comments above code

--- Assignment 1/levenslijn.js
+++ Assignment 1/levenslijn.js
@@ -1,29 +1,38 @@
 /*http://bl.ocks.org/d3noob/a22c42db65eb00d4e369*/
 /*https://github.com/d3/d3/blob/master/CHANGES.md*/
 /*https://github.com/d3/d3/blob/master/API.md#easings-d3-ease*/
-var height = 400;
+
+//Size of the rectangle
+    var height = 400;
     var width = 1450;
    
+//String to date value (year)
 var parseDataYear = d3.timeParse("%Y");
 
+// declare a tree layout
 var tree = d3.tree().size([width, height]);
 d3.json("data/data.json").get(function(error,data){
  
     var margin = {left: 50, right: 50, top: 40, bottom:0};
-   
+   //parent
     var root = d3.hierarchy(data[0]);
   
     tree(root);
+    //declare div & add div in body
     var div = d3.select("body").append("div")	
     .attr("class", "tooltip")				
     .style("opacity", 0);
+    //append the svg variable to the body
     var svg = d3.select("body").append("svg").attr("width","100%").attr("height","100%");
+    // declare chartGroup & append group element to svg
     var chartGroup = svg.append('g')    
     .attr("transform","translate("+margin.left+","+margin.top+")");
     
+    //declare year & values array
     var Year = [];
     var values = [];
     
+    //add values from json file to the Arrays
     for(x in data[0]["Vlaams-brabant"][0]){
        values.push(data[0]["Vlaams-brabant"][0][x]);
     }
@@ -31,36 +40,37 @@ d3.json("data/data.json").get(function(error,data){
        Year.push(x);
     }
     
-        
+       //Scale year values  
      var x = d3.scaleTime()
      .domain(d3.extent(Year,function(d){return parseDataYear(d);}))
      .range([0,width]);
-    
+    //Scale values
     var y = d3.scaleLinear()
     .domain(d3.extent(values,function(d){return d+10;}))
     .range([height-20,0]);
    
+    //append data to x axis & y axis
     var xAxis = d3.axisBottom(x);
     var yAxis = d3.axisLeft(y);
     
-  
+        //declare line chart
      var line = d3.line()                     
                     .x(function(d,i){return i*240;})
                     .y(function(d){return y(d);})
                     .curve(d3.curveNatural);
                
    
-    
+    //declare path & to chartGroup element
       var path = chartGroup.append("path")                    
             .attr("stroke", "green")
             .attr("stroke-width","3")
             .attr("fill","none")
             .attr("d",line(values))           
-            .on("mouseover",function(){$(".dot").css("fill","white");});
-            /*.on("mouseleave",function(){$(".dot").css("fill","none");});*/
-    
+            .on("mouseover",function(){$(".dot").css("fill","white");}); //when hovering, it appears white
+           
+                //length of path
            var totalLength = path.node().getTotalLength();
-      
+            //path animation repeat
             path            
             .transition()           
             .duration(4000)           
@@ -79,33 +89,35 @@ d3.json("data/data.json").get(function(error,data){
             
             
             
-           
+           //add values to on y axis & g element
         chartGroup.append("g")
         .attr("class","axis y")       
         .call(yAxis);
     
+          //add year values to g element & x axis
         chartGroup.append("g")
         .attr("transform","translate(0,"+height+")")
         .attr("class","axis x hidden")        
         .call(xAxis);
-         
+    
+         //select all points equal to year values 
     chartGroup.selectAll("circle").data(Year).enter()       
        .append("circle")      
         .attr("class", "dot")   
         .attr("cx",function(d,i){return i*240;} )
         .attr("cy",function(d,i){ return y(values[i]);})
         .attr("r", function(d,i) { return 5; })   
-        .on("mouseover",function(d,i){       
+        .on("mouseover",function(d,i){     //appear when hovering on path
         $("g.axis.x").show();$("g.axis.x").css("stroke","white"); console.log("hit");
         $("g.axis.y").show();$("g.axis.y").css("stroke","white"); 
-        div.transition()
+        div.transition()//animation
         .duration(200)
         .style("opacity", .9);	       
        div	.html( values[i]+ "<br/>"  + Year[i])	
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px");	
         })
-        .on("mouseout",function(){
+        .on("mouseout",function(){//disappear when mouse is not nearby
         $("g.axis.x").hide();
         $("g.axis.y").hide();
           div.transition()		
--- Assignment 2/public/app.js
+++ Assignment 2/public/app.js
@@ -1,14 +1,19 @@
+//declaration of variables
 var tweets = {};
 var settings = undefined;
+
+//get settings from settings.json
 var settingrequest = new XMLHttpRequest();
 settingrequest.open('GET', '/settings', true);
 settingrequest.onload = function () {
     settings = JSON.parse(this.response);
+    //get data from serv.js
     var datarequest = new XMLHttpRequest();
     datarequest.open('GET', '/data', true);
     datarequest.onload = function () {
         var data = JSON.parse(this.response);
         data = data.statuses;
+        //add data underneath in list
         let str = "<ul>";
         data.forEach(tweet => {
             str += "<li>" + tweet.text + "--" + tweet.retweet_count + "</li>";
@@ -16,31 +21,35 @@ settingrequest.onload = function () {
         str += "</ul>";
         document.getElementById(settings.dataid).innerHTML = str;
         tweets.data = data;
+        //declare p5 variable & s function
         var myp5 = new p5(s, settings.vizid);
     }
     datarequest.send();
 }
 settingrequest.send();
-
+//Draw Data
 var s = function (sketch) {
-    
+    //get slider from index
     var slider = document.getElementById("slider");
+    //sketch settings
     sketch.setup = function () {
         const w = document.getElementById(settings.vizid).offsetWidth;
         sketch.createCanvas(w, w);           
-        console.log(tweets);
+       
         sketch.textSize(50);
         sketch.colorMode(sketch.HSB, 255);
         sketch.textAlign(sketch.CENTER);
        
     };
+    //Drawing function
     sketch.draw = function () {
-        console.log(slider.value);
-        sketch.frameRate(parseInt(slider.value));
-        sketch.background(settings.secondarycolor);
+       
+        sketch.frameRate(parseInt(slider.value));//raise frame rate depending from slider value
+        //setting color
+        sketch.background(settings.secondarycolor); 
         sketch.fill(settings.primarycolor);
         sketch.stroke(settings.primarycolor);
-        let w = sketch.width;
+        //for loop iterate trough tweets & show them on rectangle 
         for (let i = 0; i < tweets.data.length; i++) {
             var rdm = Math.floor(Math.random() * 2000);
             var color = Math.floor(Math.random() * 255);
@@ -48,7 +57,7 @@ var s = function (sketch) {
             WordsRandom(tweets.data[i].text, i, tweets.data[i].retweet_count + rdm);
         }
     }
-
+        //Text function
     function WordsRandom(text, x, y) {
         sketch.text(text, x, y);
     }
